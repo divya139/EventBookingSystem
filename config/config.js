01fd1d7b9469af4878982d0a78ab2d6e837f6511
unknown <divya139>
2020-02-21 01:26:45 +0800

    Moved host name to config file

--- config/config.js
+++ config/config.js
@@ -3,4 +3,7 @@
 module.exports = {
   db: 'mongodb://username:password@url:port/db',
   db_dev: 'mongodb://localhost:27017/booking_system',
+  backend_host_prod:"http://3.15.175.63/",
+  backend_host_dev:"0.0.0.0",
+  port:8080
 };
--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const config = require("../config/config");
 const webpackConfig = require("../webpack.config");
 
 const isDev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 8080;
+var hostName = "";
+const port = process.env.PORT || config.port;
 
 // Configuration
 // ================================================================================================
@@ -29,6 +30,7 @@ require("./routes")(app);
 
 if (isDev) {
   const compiler = webpack(webpackConfig);
+  hostName = config.backend_host_dev;
 
   app.use(
     historyApiFallback({
@@ -54,6 +56,7 @@ if (isDev) {
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.resolve(__dirname, "../dist")));
 } else {
+  hostName = config.backend_host_prod;
   app.use(express.static(path.resolve(__dirname, "../dist")));
   app.get("*", function(req, res) {
     res.sendFile(path.resolve(__dirname, "../dist/index.html"));
@@ -61,12 +64,12 @@ if (isDev) {
   });
 }
 
-app.listen(port, "0.0.0.0", err => {
+app.listen(port,hostName, err => {
   if (err) {
     console.log(err);
   }
 
-  console.info(">>> 🌎 Open http://0.0.0.0:%s/ in your browser.", port);
+  console.info(">>> 🌎 Open ", hostName, " in your browser.", port);
 });
 
 module.exports = app;
